ab5497d0b8f7bdb0959a13f0233724017b29c114
Ian Reed <ireed3597@protonmail.com>
2025-02-13 12:51:14 -0800

    Fixes to file naming and branch handling

--- convert/convert_parquet_to_root.py
+++ convert/convert_parquet_to_root.py
@@ -49,7 +49,7 @@ parser.add_argument(
 args = parser.parse_args()
 
 """
-Script to convert parquet files into TTrees so that it can be used with the SRs optimization script.
+Script to convert parquet files into FlashggFinalFit flavor TTrees.
 """
 
 def to_tensor(dataframe, columns = [], dtypes = {}):
@@ -62,8 +62,8 @@ def to_tensor(dataframe, columns = [], dtypes = {}):
         if column not in dtypes.keys():
             if column == "proc" or column == "sample"or column =="run_era":
                 dtype_list.append("S15")
-            else: dtype_list.append(type(dataframe[column][0]))
-                
+            else:
+                dtype_list.append(type(dataframe[column][0]))
         else:
             dtype_list.append(dtypes[column])
     # Build dictionary with lists of column names and formatting in the same order
@@ -74,15 +74,13 @@ def to_tensor(dataframe, columns = [], dtypes = {}):
     # Initialize _mostly_ empty numpy array with column names and formatting
     numpy_buffer = numpy.zeros(
         shape = len(dataframe),
-        dtype = dtype_dict)
+        dtype = dtype_dict
+    )
     # Insert values from dataframe columns into numpy labels
     for col in columns:
         #print(f"column {col} input shape {numpy_buffer[col].shape} output shape {dataframe[col].to_numpy().shape}")
         numpy_buffer[col] = dataframe[col].to_numpy()
-    # Return results of conversion
     return numpy_buffer
-
-
 
 # Format output directories
 out_dir = str(args.FggFF) + 'files_systs/' + str(args.tag) + '/'
@@ -97,7 +95,6 @@ os.system("mkdir -p %s/2018"%(out_dir))
 
 # Organize binning
 nSRs = len(args.mvas)
-print(nSRs)
 args.mvas+=[99]
 args.mvas.sort(reverse=True)
 
@@ -110,35 +107,39 @@ needed_fields=['CMS_hgg_mass','weight_central','process_id']
 years = ['2017','2018']
 procs = ['ttHH_ggbb','ttH_M125']
 
+print('Started Processing Data')
 df = ak.from_parquet(str(args.input)+'merged_nominal.parquet')
 df = df[df.process_id == procs_id_map["Data"]]
+print(f'File contains {len(df)} total Data events')
 df['CMS_hgg_mass'] = df.Diphoton_mass
 
+out_file = f'{out_dir}/Data/allData.root'
 for sr in range(nSRs):
-    print(f'Processing SR {sr}')
     sr_mask = (df[args.mva_name] < args.mvas[sr]) & (df[args.mva_name] >= args.mvas[sr+1])
     peak_mask = (df.Diphoton_mass < 120) | (df.Diphoton_mass > 130)
     if args.unblind:
-        dfs = df[sr_mask]
+        df_sr = df[sr_mask]
     else:
-        dfs = df[sr_mask & peak_mask]
-    dfs = dfs[[f for f in dfs.fields if f in needed_fields]]
-    print(dfs.fields)
-    dfs = to_tensor(dfs)
-    print(f'Adding {len(dfs)} events to allData')
-    if not sr:
-        with uproot.recreate(out_dir+'/Data/allData.root') as f_out:
-            f_out['Data_13TeV_SR'+str(sr+1)] = dfs
+        df_sr = df[sr_mask & peak_mask]
+    df_sr = df_sr[[f for f in df_sr.fields if f in needed_fields]]
+    df_sr = to_tensor(df_sr)
+    print(f'Adding {len(df_sr)} events to allData SR{sr+1}')
+    if os.path.isfile(out_file):
+        with uproot.update(out_file) as f_out:
+            f_out[f'Data_13TeV_SR{sr+1}'] = df_sr
     else:
-        with uproot.update(out_dir+'/Data/allData.root') as f_out:
-            f_out['Data_13TeV_SR'+str(sr+1)] = dfs
+        with uproot.recreate(out_file) as f_out:
+            f_out[f'Data_13TeV_SR{sr+1}'] = df_sr
+print('Finished Processing Data')
+print('------------------------')
  
+print("Started Processing MC")
 # Second, open all the files and save a file per year per process with all the systematics
 files = glob.glob(str(args.input)+'/*.parquet')
-firstRound = True
 for f_in in files:
+    print(f'Started Processing {f_in.split("/")[-1]}')
     df = ak.from_parquet(f_in)
-    print(f'{len(df)} total entries')
+    print(f'File contains {len(df)} total entries')
     # Build unique TTree names
     tag = ''
     if 'nominal' not in f_in.split("/")[-1]:
@@ -154,13 +155,13 @@ for f_in in files:
         tag = '_MCScale' + tag
     if 'smear' in f_in.split("/")[-1]:
         tag = '_MCSmear' + tag
-        df['CMS_hgg_mass'] = df['Diphoton_mass']
-        df['weight'] = df['weight_central'] * 2
-        df['weight_central'] = df['weight']
 
-    yield_systematics = [ field for field in df.fields if ( "weight_" in field ) and ( "_up" in field or "_down" in field )]
+    df['CMS_hgg_mass'] = df['Diphoton_mass']
+    df['weight'] = df['weight_central'] * 2
+    df['weight_central'] = df['weight']
+
+    yield_systematics = [field for field in df.fields if ("weight_" in field) and ("_up" in field or "_down" in field)]
     # A bit of gymnastics to get the inputs right for Mr. flashggFinalFit
-    #TODO: Maybe consider renaming during the preselection/skimming?
     rename_syst = {}
     for syst in yield_systematics:
         if "_up" in syst:
@@ -188,77 +189,34 @@ for f_in in files:
                 continue
             rename_syst[syst] = syst.replace("_down","Down01sigma")
         df[rename_syst[syst]] = df[syst]
+    needed_fields+=rename_syst.values()
 
-    #for sr in range(nSRs):
-    #    dfs = df.loc[ ( df[args.mva_name] < args.mvas[sr] ) & ( df[args.mva_name] >= args.mvas[sr+1] ) & (df.event % 2 == 1) ]
-    #    print("Adding {} events to {}".format(len(dfs),year_str+"_"+proc_tag))
-    #    dfs.to_root(out_dir+year_str+'/'+proc_tag+'_125.38_13TeV.root',''+proc_tag+'_125.38_13TeV_SR'+str(sr+1)+tag, mode='a')
     for y in years:
-        dfy = df[df.year == y]
+        print('----------------------------')
+        print(f'Started Processing year {y}')
+        df_year = df[df.year == y]
         for sr in range(nSRs):
-            print(f'Processing SR {sr}')
             # Test/Train/Validation split
             #TODO: Match one used in the Run3 SR optimizaiton
-            sr_mask = (dfy[args.mva_name] < args.mvas[sr]) & (dfy[args.mva_name] >= args.mvas[sr+1]) & (dfy.event % 2 == 1) 
-            peak_mask = (dfy.Diphoton_mass < 120) | (dfy.Diphoton_mass > 130)
+            sr_mask = (df_year[args.mva_name] < args.mvas[sr]) & (df_year[args.mva_name] >= args.mvas[sr+1]) & (df_year.event % 2 == 1) 
+            peak_mask = (df_year.Diphoton_mass < 120) | (df_year.Diphoton_mass > 130)
             if args.unblind:
-                dfs = dfy[sr_mask]
+                df_sr = df_year[sr_mask]
             else:
-                dfs = dfy[sr_mask & peak_mask]
-            dfs = dfs[[f for f in dfs.fields if f in needed_fields]]
+                df_sr = df_year[sr_mask & peak_mask]
+            df_sr = df_sr[[f for f in df_sr.fields if f in needed_fields]]
 
             for p in procs:
-                dfo = dfs[dfs.process_id==procs_id_map[p]] 
-                dfo = to_tensor(dfo)
-                print(f'Adding {len(dfo)} entires to {p} {y}')
-                if firstRound:
-                    with uproot.recreate(out_dir+f'/{y}/{p}_125.38_13TeV.root') as f_out:
-                        f_out[f'{p}_125.38_13TeV_SR'+str(sr+1)+tag] = dfo
+                df_out = df_sr[df_sr.process_id==procs_id_map[p]] 
+                df_out = to_tensor(df_out)
+                print(f'Adding {len(df_out)} entires to {p} {y} SR{sr+1}')
+                out_file = f'{out_dir}/{y}/{p}_125.38_13TeV.root'
+                if os.path.isfile(out_file):
+                    with uproot.update(out_file) as f_out:
+                        f_out[f'{p}_125.38_13TeV_SR{sr+1}{tag}'] = df_out
                 else:
-                    with uproot.update(out_dir+f'/{y}/{p}_125.38_13TeV.root') as f_out:
-                        f_out[f'{p}_125.38_13TeV_SR'+str(sr+1)+tag] = dfo
-    firstRound = False
- 
-
-#
-#
-#
-#
-#events = ak.from_parquet(args.input)
-#
-## For the time being, we have to match processes into integers. Will need to modify the SR opt FIXME  
-#proc_dict={
-#    "Data_EraE": 0, "Data_EraF": 0, "Data_EraG": 0, "Data": 0,
-#    "GluGluHToGG": 1, "ttHToGG": 2, "VBFHToGG": 3, "VHToGG": 4,
-#    "GGJets": 5, "DDQCDGJets": 6,"DDBKG": 6, 
-#    "GluGluToHH": 8  
-#}
-#
-#if "proc" in events.fields: #SnT style parquet
-#  events = ak.with_field(events, [ proc_dict.get(proc) for proc in events["proc"].to_list()] , "process_id")
-#  needed_fields=["mass","weight","score_GluGluToHH","process_id"]
-#
-#elif "sample" in events.fields: #NW style parquet
-#  events = ak.with_field(events, [ proc_dict.get(proc) for proc in events["sample"].to_list()] , "process_id")
-#  needed_fields=["pt","mass","weight_tot","signleH_dnn_new","ddbkg_dnn","process_id"]
-#else:
-#    sys.exit("[Parquet2root] Parquet format not supported (no proc or sample field found)")
-#    
-#print ("[Parquet2root] Done with the id addition")
-#
-#if args.slim: # Slimming by defaut. It's good for you + NW parquet conversion not fully working for literal fields FIXME
-#    events = events[[f for f in events.fields if f in needed_fields]]
-#    print("[Parquet2root] Done with the slimming")
-#
-##square down parquet dataframes before exporting to root
-#events = to_tensor(events)
-#
-#if args.out_name is None:
-#    output_root = args.out_dir + args.input.split("/")[-1].replace(".parquet", ".root")
-#else:
-#    output_root = args.out_dir + args.out_name + ".root"
-#
-#with uproot.recreate(output_root) as f:
-#  f["t"] = events
-#  
-#print("[Parquet2root] All good")
+                    with uproot.recreate(out_file) as f_out:
+                        f_out[f'{p}_125.38_13TeV_SR{sr+1}{tag}'] = df_out
+    needed_fields = needed_fields[:-len(rename_syst.values())]
+    print('----------------------------')
+print('All files processed')
